c62b6adab3c98396eef0e9ae3906cb35ec2983c3
aini darojat <ayniidarojat99@gmail.com>
2018-01-21 11:48:00 +0700

    first commit

---
+++ frontend/views/site/about.php
@@ -0,0 +1,20 @@
+<?php
+
+/* @var $this yii\web\View */
+
+use yii\helpers\Html;
+
+$this->title = 'Tentang';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="site-about">
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>This is the About page. my message for you :</p>
+
+<span style="color: blue; font-family: Los Blau Erreur; font-size: 30px;">
+	Thanks for : <br>
+1. Bp. KHANIF ZYEN TERCINTA <br>
+2. Mas HUDA TERSAYANG </span> 
+    
+</div>
